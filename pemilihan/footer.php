93f98caf2e01ac233a054c8af73486f181f17a5e
iqbal gilang permana <64182565+pangkrek@users.noreply.github.com>
2025-07-30 08:56:20 +0700

    Delete pemilihan directory

--- pemilihan/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-light text-center text-lg-start mt-5">
-    <div class="text-center p-3">
-        © <?php echo date("Y"); ?> Hak Cipta : <a class="text-dark" href="#"> Iqbal Gilang Permana, S.Kom </a>
-    </div>
-</footer>
--- pemilihan/generate_hash.php
+++
@@ -1,6 +0,0 @@
-   <?php
-   $password = 'password'; // Ganti dengan password yang diinginkan
-   $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-   echo $hashedPassword; // Salin hasil hash ini
-   ?>
-   
